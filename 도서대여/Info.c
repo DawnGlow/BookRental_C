c52148af2f430ca8dd6e0c74979afbc326dd78cb
DawnGlow <ferrater1013@gmail.com>
2020-08-20 18:48:28 +0900

    Admin Finish

--- 도서대여/Info.c
+++ 도서대여/Info.c
@@ -2,6 +2,10 @@
 #include "Info.h"
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#pragma warning(disable : 4996)
+bookinfo b[30] = { 0, };
 
 void clearBuffer(void) {
     while (getchar() != '\n');
@@ -74,8 +78,13 @@ void password()
 
 void listload()
 {
-    bookinfo b[30] = { 0, };
+    printf("bookinfo.txt에서 책 정보를 가져오는 중입니다\n");
     FILE* fp = fopen("bookinfo.txt", "r");
+    if (fp == NULL)
+    {
+        printf("파일을 불러오는데 실패하였습니다\n");
+        exit(1);
+    }
     for (int i = 0; i < 3; i++)
     {
         fgets(b[i].title, 50, fp);
@@ -88,4 +97,57 @@ void listload()
     }
     fclose(fp);
 
+}
+
+void listadd()
+{
+    for (int i = 0; i < 3; i++) {
+        if (strlen(b[i].title) == 0)
+        {
+            int k;
+            printf("제목 입력 : \n");
+            gets_s(b[i].title, sizeof(b[i].title));
+            printf("ISBN 입력 : \n");
+            gets_s(b[i].ISBN, sizeof(b[i].ISBN));
+            printf("작가 입력 : \n");
+            gets_s(b[i].author, sizeof(b[i].author));
+            printf("출판사 입력 : \n");
+            gets_s(b[i].publishhouse, sizeof(b[i].publishhouse));
+            printf("출판년도 입력 : \n");
+            gets_s(b[i].publishyear, sizeof(b[i].publishyear));
+            printf("카테고리 입력 : \n");
+            gets_s(b[i].category, sizeof(b[i].category));
+            printf("대출가능여부 입력 : \n");
+            gets_s(b[i].land, sizeof(b[i].land));
+
+            printf("%d번째 책 등록에 성공했습니다. 관리자 메뉴로 돌아가려면 1번을 입력해주세요, 계속 등록하려면 1번을 제외한 아무 문자를 입력해주세요\n", i + 1);
+            scanf("%d", &k);
+            if (k == 1)
+                break;
+            clearBuffer();
+        }
+        else
+            continue;
+    }
+
+}
+
+void listdelete()
+{
+    int* arr = (int*)malloc(sizeof(int) * 4);
+    printf("ISBN을 입력해주세요(4자리) : ");
+    scanf("%d", arr);
+    for (int i = 0; i < 3; i++)
+    {
+        while ((*arr) == atoi(b[i].ISBN))
+        {
+            memset(&b[i], 0, sizeof(bookinfo));
+        }
+    }
+    printf("삭제하였습니다\n");
+    //아래 코드는 테스트용 코드입니다.
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%s %s %s %s %s %s %s\n", b[i].title, b[i].ISBN, b[i].author, b[i].publishhouse, b[i].publishyear, b[i].category, b[i].land);
+    }
 }
--- 도서대여/main.c
+++ 도서대여/main.c
@@ -13,7 +13,6 @@ int main(void)
 	}
 	else {
 		printf("학생 모드를 실행합니다\n");
-		studentmenu();
 	}
 
 	return 0;
